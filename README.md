9f6cd764a7166d0b77069f9a94ff2598cb2bb913
Drew Olsen <olsenj.andrew@gmail.com>
2021-03-17 21:57:19 -0500

    Initial setup of files

---
+++ README.md
@@ -0,0 +1,10 @@
+# 05 Work Day Scheduler
+
+## Your Task
+
+[Github Pages](https://momentjs.com/)
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
---
+++ assets/js/scripts.js
@@ -0,0 +1,28 @@
+/* 
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+ */
+
+
+
+/* 
+TODO: Figure out moment.js
+TODO: have the top of the page information generated and placed on the page
+TODO: create a work day object with time and value blocks
+TODO: create a function to create the work day structure on the page that it grabs from
+TODO: Figure out logic to see if day is in past or future or current time. - This is for the color coding
+TODO: Make click function for the day blocks save button. Reference the event delegation jquery inclass activity
+
+
+*/
