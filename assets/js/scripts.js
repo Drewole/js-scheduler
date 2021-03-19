bd843518e911ad507ddac7690ea51efd96e2c754
Drew Olsen <olsenj.andrew@gmail.com>
2021-03-19 01:35:56 -0500

    Added logic to determine item class

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,52 +1,42 @@
 
 
-// Our moment js call should be something like this
-let momentTime = moment().format('LT');
+// This grabs the current time for comparison below
+function currentTime() {
+	let currentTime = moment().format('h');
+	return currentTime;
+}
 
-// var fullDaySchedule = {
-// 	H900 : {
-// 		dayEvents: "",
-// 		timePeriod: "AM"
-// 	},
-// 	H1000 : {
-// 		dayEvents: "",
-// 		timePeriod: "AM"
-// 	},
-// 	H1100 : {
-// 		dayEvents: "",
-// 		timePeriod: "AM"
-// 	},
-// 	H1200 : {
-// 		dayEvents: "",
-// 		timePeriod: "PM"
-// 	},
-// 	H100 : {
-// 		dayEvents: "",
-// 		timePeriod: "PM"
-// 	},
-// 	H200 : {
-// 		dayEvents: "",
-// 		timePeriod: "PM"
-// 	},
-// 	H300 : {
-// 		dayEvents: "",
-// 		timePeriod: "PM"
-// 	},
-// 	H400 : {
-// 		dayEvents: "",
-// 		timePeriod: "PM"
-// 	},
-// 	H500 : {
-// 		dayEvents: "",
-// 		timePeriod: "PM"
-// 	}
-// }
+// Function to generate time array with given start and end point.
+function letsMakeTimeArray(start, end) {
+	let timeArray = [];
+	for (time = start; time < (end + 1); time++) { // Need to add 1 to the end var to compensate for the 0 start index
+		if (time > 12) {
+			var j = time - 12;
+		} else {
+			j = time;
+		}
+		var hourBlock =
+		{
+			hour: j,
+			ampm: "AM",
+			toDos: ""
+		}
+		if (time > 11) {
+			hourBlock.ampm = "PM";
+		}
+		timeArray.push(hourBlock);
 
-//globals, should we mess with jquery?
+	}
+	return timeArray;
+}
+let timeArray = letsMakeTimeArray(9, 17); // 1 - 24 scale, I think the intent is obvious. First number is start time, second is end time
+
+//globals, should we mess with jquery? -- Decided not to
 const containerEL = document.querySelector(".container");
 
 // Retrive any data from local storage
-var fullDaySchedule = JSON.parse(localStorage.getItem("schedule"));
+// var timeArray = JSON.parse(localStorage.getItem("schedule"));
+
 
 
 /*
@@ -68,65 +58,63 @@ THEN the saved events persist
 
 
 /*
-TODO: Figure out moment.js
+DONE: Figure out moment.js
 DONE: have the top of the page information generated and placed on the page
-TODO: create a work day object with time and value blocks
-TODO: create a function to create the work day structure on the page that it grabs from
-TODO: Figure out logic to see if day is in past or future or current time. - This is for the color coding
-TODO: Make click function for the day blocks save button. Reference the event delegation jquery inclass activity
+DONE: create a work day object with time and value blocks
+DONE: create a function to create the work day structure on the page that it grabs from
+DONE: Figure out logic to see if day is in past or future or current time. - This is for the class color coding
+TODO: Make click function for the day blocks save button. Reference the event delegation jquery inclass activity. On click should grab all info, update the array and send it on up.
 
 
 */
 
 
 
-// Send to local storage
-function sendToLocalStorage( items ) {
-	localStorage.setItem("schedule", JSON.stringify(items));
-	console.log("Local storage has been updated.")
-}
 
-
-
-// Function to generate time array with given start and end point.
-function letsMakeTimeArray(start,end) {
-	let timeArray = [];
-	for (time = start; time < (end+1); time++) { // Need to add 1 to the end var to compensate for the 0 start index
-		if (time > 12) {
-			var j = time - 12;
-		} else {
-			j = time;
-		}
-		var hourBlock =
-			{
-				hour: j,
-				ampm: "AM",
-				toDos: ""
-			}
-		if (time > 11) {
-			hourBlock.ampm = "PM";
-		}
-		timeArray.push(hourBlock);
-		
-	}
-	return timeArray;
-}
-let timeArray = letsMakeTimeArray(9,17); // 1 - 24 scale, I think the intent is obvious. First number is start time, second is end time
-
-sendToLocalStorage(timeArray);
-console.log(timeArray)
-
-
-// Lets cycle through the data and create the items on the screen
-//In the logic, need to add classes on the text area to have the right colors
-let timeBlock = `
-<div data-time="900" class="time-block row"><div class="time-display"></div><textarea class="present"></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>
-`
 
 //Lets put the time and date up in the header
-function dayAndDateHeader(){
+function dayAndDateHeader() {
 	let date = moment().format("dddd, MMMM Do");
 	document.querySelector("#currentDay").textContent = date;
 }
 dayAndDateHeader();
 
+
+
+// Go through our array and put it on the page plz
+function makeTimeSlots(arr) {
+
+	for (i = 0; i < arr.length; i++) {
+
+		//Lets do some logic to see what class we need to be applying to the textarea
+		let itemClass = "past";
+		if (arr[i].hour > currentTime) {
+			itemClass = "future";
+		} else if (arr[i].hour === currentTime){
+			itemClass = "preesent";
+		}
+
+		//This will be our HTML
+		let hourTemplate = `<div class="time-display col-1">${arr[i].hour} ${arr[i].ampm}</div><textarea class="${itemClass} col-10">${arr[i].toDos}</textarea><button class="saveBtn col-1"><i class="fas fa-save"></i></button>`;
+
+		//Lets create the element, add some classes and set some attributes, then add it to the bottom of the container div
+		var timeBlock = document.createElement("div");
+		timeBlock.innerHTML = hourTemplate;
+		timeBlock.classList.add("time-block" , "row");
+		timeBlock.setAttribute("data-time", `${arr[i].hour}`);
+		
+		containerEL.appendChild(timeBlock);
+	}
+	
+
+}
+makeTimeSlots(timeArray); // Calling the function with our desired array
+
+
+
+// Send to local storage
+function sendToLocalStorage(items) {
+	localStorage.setItem("schedule", JSON.stringify(items));
+	console.log("Local storage has been updated.")
+}
+sendToLocalStorage(timeArray); // Sending the time arry to local storage
